d42be633bf8b0323eedea9bf2a090fc17efe4919
Juliana Aigueira <121833579+JulianaAigueira@users.noreply.github.com>
2023-03-01 15:23:47 -0300

    Tela inicial
    Ajustando a tela pra redimensionar automaticamente.

--- moeda.py
+++ moeda.py
@@ -4,13 +4,18 @@ janela = tk.Tk()
 
 janela.title('COTAÇÃO DE MOEDAS')
 
-mensagem = tk.Label(text='Sistema de Busca de Cotação de Moedas', fg='white', bg='black', width=35, height=5)
-mensagem.pack()
+#ajustar a tela automáticamente
 
-mensagem = tk.Label(text='Selecione a moeda desejada')
-mensagem.pack()
+janela.rowconfigure(0, weight=1)
+janela.columnconfigure([0, 1], weight=1)
+
+mensagem = tk.Label(text='Sistema de Busca de Cotação de Moedas', fg='white', bg='black', width=35, height=5)
+mensagem.grid(row=0, column=0, columnspan=2, sticky='NSEW')#NSEW - norte, sul, leste, oeste
+
+mensagem2 = tk.Label(text='Selecione a moeda desejada')
+mensagem2.grid(row=1, column=0)
 
 moeda = tk.Entry()
-moeda.pack()
+moeda.grid(row=1, column=1)
 
 janela.mainloop()
